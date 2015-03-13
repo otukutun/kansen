8291e47ab2ae3652a9eddf08319cfa7fe1ebb4cc
otukutun <orehaorz@gmail.com>
2015-03-14 02:46:17 +0900

    Add team validation

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,9 @@ class Match < ActiveRecord::Base
   belongs_to :home_team_id, foreign_key: :team_id
   belongs_to :away_team_id, foreign_key: :team_id
   belongs_to :league_id
+  
+  validate :home_team_id, presence: true
+  validate :away_team_id, presence: true
+  validate :flg, presence: true
+  validate :league_id, presence: true
 end
