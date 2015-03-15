ee80e31bddc726e30be1946956f6f5e608edc8bb
otukutun <orehaorz@gmail.com>
2015-03-15 10:58:05 +0900

    fix association

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,8 +1,8 @@
 class Match < ActiveRecord::Base
-  extend ActiveHash::Associations
-  belongs_to :team, foreign_key: :home_team_id, primary_key: :team_id
-  belongs_to :team, foreign_key: :away_team_id, primary_key: :team_id
-  belongs_to :league,foreign_key: :league_id
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :home_team, class_name: 'Team'
+  belongs_to_active_hash :away_team, class_name: 'Team'
+  belongs_to_active_hash :league
 
   validates :home_team_id, presence: true
   validates :away_team_id, presence: true
