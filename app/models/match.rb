cae9ef71c65c04d08fc0f1398a8fd851e8ef79f0
otukutun <orehaorz@gmail.com>
2015-03-15 17:30:19 +0900

    Add home_team & away_team scope to match model.

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,8 +4,20 @@ class Match < ActiveRecord::Base
   belongs_to_active_hash :away_team, class_name: 'Team', shortcuts: %i(name api_name)
   belongs_to_active_hash :league
 
+  enum flg: %i(plan end)
+
   validates :home_team_id, presence: true
   validates :away_team_id, presence: true
   validates :flg, presence: true
   validates :league_id, presence: true
+
+  scope :home, ->(home_team_api_name) {
+    home_team = Team.find_by_api_name(home_team_api_name)
+    where('home_team_id = ?', home_team.id)
+  }
+
+  scope :away, ->(away_team_api_name) {
+    away_team = Team.find_by_api_name(away_team_api_name)
+    where('away_team_id = ?', away_team.id)
+  }
 end
